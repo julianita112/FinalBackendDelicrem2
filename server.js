a90055b039a7fbd5ef4e773b94d7428057d7a4f1
julianita112 <juliananbetancur@gmail.com>
2024-10-10 22:33:52 -0500

    patch

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ dotenv.config();
 // Configuración de CORS para permitir todos los orígenes
 app.use(cors({
   origin: 'https://delicrem-fm7p.onrender.com', // Permitir todos los orígenes
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Métodos HTTP permitidos
   credentials: true, // Permitir el envío de cookies y encabezados de autenticación
 }));
 
